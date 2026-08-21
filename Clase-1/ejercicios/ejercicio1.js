48db99a3960185a88b45248bb89d9246dcaaf11b
Matias Duro <matias.duro@ina.edu.ar>
2026-08-20 21:34:37 -0300

    clase 1

---
+++ Clase-1/ejercicios/ejercicio1.js
@@ -0,0 +1,42 @@
+// Identificar palabras Palindromas
+
+let palabras = ["Casa", "Numero", "Oso", "Neuquen", "Reconocer", "Ojo", "Atun"]
+
+const BuscarPalindromo = (palabras) => {
+
+    let Palindromas = []
+
+    // Debo recorrer el array de palabras
+    // Devolver un array nuevo con las palabras Palindromas
+
+
+
+    // paso previo: lograr hacer un console log, de cada palabra
+    // verificar si se lee de atras hacia adelante igual, 
+    // Pero hay una mayuscula distinta
+    // no va a ser igual
+
+    // Para evitar el problema de la mayuscula, hay que "Sanatizar"
+
+    // Para pasar todo a mayuscula .toUpperCase()
+    // para pasar todo a minuscula .toLowerCase()
+
+    palabras.forEach(palabra => {
+        let palabraSanatizada = palabra.toLowerCase()
+
+
+        if(true){ // true hay que reemplazarlo por la condicion que evalua palabra == palabra
+
+            // agregar si cumple al array Palindromas
+        }
+    });
+    // 1- Separar cada palabra en letras
+    // 2- Invertir las palabras
+    // 3- Volver a unir las palabras
+    // 4- Compara palabra == palabraInvertida
+
+
+    return Palindromas
+}
+
+console.log(BuscarPalindromo(palabras));
--- Clase-1/variables.js
+++ Clase-1/variables.js
@@ -1,7 +1,29 @@
-let nombre
+let nombre = "Lucas"
+// CUando declaro una variable suelta, osea que no esta en una funcion ni nada adentro
+// se lo conoce como variable global
+// no es una buena practica
 
-console.log(nombre);
+// Esto que les comento, de donde vive una variable, se lo conoce como Scope
+function saludo(){
+    let nombre = "lautaro"
+    let edad = 30
+    if(true){
+        let nombre = "Mateo"
+    }
+    return nombre
+}
+// console.log(edad);
 
-nombre = "Ayelen"
+// console.log(saludo());
 
-console.log(nombre);
+
+
+function calcularEdad(){
+    return edad
+}
+
+
+console.log(calcularEdad());
+var edad = 30
+
+
