fef95001d3685dccc369067f517ab03dfbfed923
Matias Duro <matias.duro@ina.edu.ar>
2026-08-27 21:13:06 -0300

    ejercicio 1

---
+++ Clase-2/ejercicios/ejercicio1.js
@@ -0,0 +1,31 @@
+
+const usuarios = [
+    { nombre: "Ana", sexo: "F"},
+    { nombre: "Facu", sexo: "M"},
+    { nombre: "Sofia", sexo: "F"},
+    { nombre: "Manuel", sexo: "M"},
+    { nombre: "Martina", sexo: "F"},
+    { nombre: "Lucas", sexo: "M"},
+    { nombre: "Silvina", sexo: "F"},
+]
+
+
+const separarUsuarios = (usuario) => {
+
+    // inicializar las variables de hombres y mujeres
+
+    //  SI O SI CON EL FOR OF
+
+
+    
+}
+
+const resultado = separarUsuarios(usuarios)
+
+console.log(resultado);
+// lo que deberia decir la respueta
+// devolverme un objeto, con llaves mujeres y hombres
+{
+    mujeres: [{}, {}]
+    hombres: [{}, {}]
+}
